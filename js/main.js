cc88540626a5d5dba900ee4ac48b87b26b4491e9
Antonlopex <antonlopez2004@gmail.com>
2025-04-30 13:56:22 +0200

    Menu hamburguesa corregido, falta acabar de poner info en equipos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleBtn = document.querySelector('.menu-toggle')
-    const navMenu = document.querySelector('.nav-menu')
-  
-    toggleBtn.addEventListener('click', () => {
-      navMenu.classList.toggle('active')
-    })
-  })
+  const toggleBtn = document.querySelector('.menu-toggle')
+  const navMenu = document.querySelector('.nav-menu')
+
+  // Verificamos si ya existe un listener para evitar la duplicación
+  if (!toggleBtn.hasAttribute('data-listener')) {
+    toggleBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      navMenu.classList.toggle('active');
+    });
+
+    // Marcamos el botón para evitar que se añadan más listeners
+    toggleBtn.setAttribute('data-listener', 'true');
+  }
+});
