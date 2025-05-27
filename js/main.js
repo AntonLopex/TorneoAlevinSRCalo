3d881b5c7d545e75f9f26b077a99391fcfdf5334
Antonlopex <antonlopez2004@gmail.com>
2025-05-27 21:17:39 +0200

    Arreglo tabla de comidas y animaciones en el header

--- js/main.js
+++ js/main.js
@@ -5,12 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // Verificamos si ya existe un listener para evitar la duplicación
   if (!toggleBtn.hasAttribute('data-listener')) {
     toggleBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      navMenu.classList.toggle('active');
-    });
+      event.preventDefault()
+      event.stopPropagation()
+
+      // Alterna visibilidad del menú
+      navMenu.classList.toggle('active')
+
+      // Alterna clase 'open' para cambiar el icono hamburguesa a X
+      toggleBtn.classList.toggle('open')
+    })
 
     // Marcamos el botón para evitar que se añadan más listeners
-    toggleBtn.setAttribute('data-listener', 'true');
+    toggleBtn.setAttribute('data-listener', 'true')
   }
-});
+})
+
