8fd3817fd3ab779372dd1193f6f1763046e0d2f7
Antonlopex <antonlopez2004@gmail.com>
2025-05-29 19:42:19 +0200

    Menu movil arreglado, falta animacion

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleBtn = document.querySelector('.menu-toggle')
-  const navMenu = document.querySelector('.nav-menu')
+// document.addEventListener('DOMContentLoaded', () => {
+//   const toggleBtn = document.querySelector('.menu-toggle')
+//   const navMenu = document.querySelector('.nav-menu')
 
-  // Verificamos si ya existe un listener para evitar la duplicación
-  if (!toggleBtn.hasAttribute('data-listener')) {
-    toggleBtn.addEventListener('click', (event) => {
-      event.preventDefault()
-      event.stopPropagation()
+//   // Verificamos si ya existe un listener para evitar la duplicación
+//   if (!toggleBtn.hasAttribute('data-listener')) {
+//     toggleBtn.addEventListener('click', (event) => {
+//       event.preventDefault()
+//       event.stopPropagation()
 
-      // Alterna visibilidad del menú
-      navMenu.classList.toggle('active')
+//       // Alterna visibilidad del menú
+//       navMenu.classList.toggle('active')
 
-      // Alterna clase 'open' para cambiar el icono hamburguesa a X
-      toggleBtn.classList.toggle('open')
-    })
+//       // Alterna clase 'open' para cambiar el icono hamburguesa a X
+//       toggleBtn.classList.toggle('open')
+//     })
 
-    // Marcamos el botón para evitar que se añadan más listeners
-    toggleBtn.setAttribute('data-listener', 'true')
-  }
-})
+//     // Marcamos el botón para evitar que se añadan más listeners
+//     toggleBtn.setAttribute('data-listener', 'true')
+//   }
+// })
 
